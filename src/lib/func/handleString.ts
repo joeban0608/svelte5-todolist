b11b56d92002b52338fbc42caadcaffe5497ab3f
joeban <joeanddaneil5@gmail.com>
2024-12-10 17:58:35 +0800

    refactor: firstWordUpperCaseAndJoinSpace func

--- src/lib/func/handleString.ts
+++ src/lib/func/handleString.ts
@@ -1,9 +1,8 @@
-
 export function firstWordUpperCaseAndJoinSpace(link: string | null) {
-	return (link ?? '')
-		.split('/')
-		.pop()
-		?.split('-')
+	if (!link || !link.split('/').pop()) return '';
+	const linkPop = link.split('/').pop() as string;
+	return linkPop
+		.split('-')
 		.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
 		.join(' ');
 }
